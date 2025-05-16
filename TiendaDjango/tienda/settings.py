a736be9fb23ede299fd6bb50f584882031c1b4ae
pipesama500 <felipemartinezc004@gmail.com>
2025-05-16 03:43:30 -0500

    Docker1

--- TiendaDjango/tienda/settings.py
+++ TiendaDjango/tienda/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'tienda.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.sqlite3'),
+        'NAME':   os.getenv('DB_NAME', BASE_DIR / 'db.sqlite3'),
+        'USER':   os.getenv('DB_USER', ''),
+        'PASSWORD': os.getenv('DB_PASSWORD', ''),
+        'HOST':   os.getenv('DB_HOST', ''),
+        'PORT':   os.getenv('DB_PORT', ''),
     }
 }
 
@@ -129,6 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
