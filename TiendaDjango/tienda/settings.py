3c159608be95578c2736cea725c127f8c46db6ee
Camus0023 <juanchomejia0317@gmail.com>
2025-05-16 01:18:36 -0500

    api phrase

--- TiendaDjango/tienda/settings.py
+++ TiendaDjango/tienda/settings.py
@@ -63,10 +63,12 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'core.context_processors.daily_quote',
             ],
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'tienda.wsgi.application'
 
